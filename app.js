06763df4f54ce9c318f69b1e8ae79b202a53ff60
SAIF UR REHMAN <155957519+saif123-wc@users.noreply.github.com>
2024-01-25 14:50:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let age=('24 yers old');
+var isstudent=('i am a web developer');
+   console.log(age);
+   console.log(isstudent);
+
+ let intro= alert('WELCOME TO MY WEB PAGE. MY NAME IS SAIF UR REHMAN.I DID MY GRADUATION FROM KARACHI UNIVERSITY AND CURRENTLY I AM DOING WEB DEVELOPER COURSE FROM BANO QABIL INSTITUTE.');
